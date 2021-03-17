191380c00fa2287c3fb550c3961446986f2823b2
rahin <k20034059@kcl.ac.uk>
2021-03-17 22:13:53 +0000

    Building up the stucture - rightbnt not working

---
+++ CW4 - EarthBnB/src/project_files/StatC.java
@@ -0,0 +1,29 @@
+package project_files;
+
+import java.util.ArrayList;
+
+/**
+ * Class AccommodationTypeStat is the statistic of how many properties in the set of data are of the type "entire home and apartments"
+ */
+public class StatC extends Statistic {
+
+    /**
+     * Create an object for the statistic counting the number of entire home and apartments listed.
+     */
+    public StatC(ArrayList<AirbnbListing> listings)
+    {
+        name = "Number of entire home and apartments";
+        updateStatistic(listings);
+    }
+
+    /**
+     * Update the statistic.
+     * @param listings A list of listings the statistic should be calculated for.
+     * @return
+     */
+    protected String updateStatistic(ArrayList<AirbnbListing> listings)
+    {
+        return "Hello world"; //used to get it to run - need changing
+
+    }
+}
--- CW4 - EarthBnB/src/project_files/StatisticsPanel.java
+++ CW4 - EarthBnB/src/project_files/StatisticsPanel.java
@@ -11,10 +11,17 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class StatisticsPanel extends Panel {
+    //all the different statistics
     Statistic accommodationType;
     Statistic availableProperties;
     Statistic averageNumReviews;
     Statistic mostExpensiveBorough;
+    //dummy stats - used for now to be able to implement structure
+    Statistic statA;
+    Statistic statB;
+    Statistic statC;
+    Statistic statD;
+
     ArrayList<AirbnbListing> listings;
 
     LinkedList<Statistic> panel1, panel2, panel3, panel4;
@@ -55,7 +62,57 @@ public class StatisticsPanel extends Panel {
         availableProperties = new StatAvailableProperties(listings);
         averageNumReviews = new StatAverageNumReviews(listings);
         mostExpensiveBorough = new StatMostExpensiveBorough(listings);
+        statA = new StatA(listings);
+        statB = new StatB(listings);
+        statC = new StatC(listings);
+        statD = new StatD(listings);
+
+
         this.listings = listings;
+        //create list objects for each panel
+        panel1 = new LinkedList<Statistic>();
+        panel2 = new LinkedList<>();
+        panel3 = new LinkedList<>();
+        panel4 = new LinkedList<>();
+
+        //store the stats in the lists for each panel. Hard coded for now, try find better way later if possible
+        panel1.add(accommodationType);
+        panel1.add(availableProperties);
+        panel1.add(averageNumReviews);
+        panel1.add(mostExpensiveBorough);
+        panel1.add(statA);
+        panel1.add(statB);
+        panel1.add(statC);
+        panel1.add(statD);
+
+        panel2.add(accommodationType);
+        panel2.add(availableProperties);
+        panel2.add(averageNumReviews);
+        panel2.add(mostExpensiveBorough);
+        panel2.add(statA);
+        panel2.add(statB);
+        panel2.add(statC);
+        panel2.add(statD);
+
+        panel3.add(accommodationType);
+        panel3.add(availableProperties);
+        panel3.add(averageNumReviews);
+        panel3.add(mostExpensiveBorough);
+        panel3.add(statA);
+        panel3.add(statB);
+        panel3.add(statC);
+        panel3.add(statD);
+
+        panel4.add(accommodationType);
+        panel4.add(availableProperties);
+        panel4.add(averageNumReviews);
+        panel4.add(mostExpensiveBorough);
+        panel4.add(statA);
+        panel4.add(statB);
+        panel4.add(statC);
+        panel4.add(statD);
+
+
     }
 
 
@@ -66,28 +123,36 @@ public class StatisticsPanel extends Panel {
         {
             case "nextBtn1": displaySinglePanel(statNameLbl1, statLbl1, getNext(statLbl1));
                 break;
-            case "prevBtn1": displaySinglePanel(statNameLbl1, statLbl1, accommodationType);
+            case "prevBtn1": displaySinglePanel(statNameLbl1, statLbl1, getPrev(statLbl1));
                 break;
-            case "nextBtn2": displaySinglePanel(statNameLbl2, statLbl2, accommodationType);
+            case "nextBtn2": displaySinglePanel(statNameLbl2, statLbl2, getNext(statLbl2));
                 break;
-            case "prevBtn2": displaySinglePanel(statNameLbl2, statLbl2, availableProperties);
+            case "prevBtn2": displaySinglePanel(statNameLbl2, statLbl2, getPrev(statLbl2));
                 break;
-            case "nextBtn3": displaySinglePanel(statNameLbl3, statLbl3, accommodationType);
+            case "nextBtn3": displaySinglePanel(statNameLbl3, statLbl3, getNext(statLbl3));
                 break;
-            case "prevBtn3": displaySinglePanel(statNameLbl3, statLbl3, accommodationType);
+            case "prevBtn3": displaySinglePanel(statNameLbl3, statLbl3, getPrev(statLbl3));
                 break;
-            case "nextBtn4": displaySinglePanel(statNameLbl4, statLbl4, accommodationType);
+            case "nextBtn4": displaySinglePanel(statNameLbl4, statLbl4, getNext(statLbl4));
                 break;
-            case "prevBtn4": displaySinglePanel(statNameLbl4, statLbl4, accommodationType);
+            case "prevBtn4": displaySinglePanel(statNameLbl4, statLbl4, getPrev(statLbl4));
                 break;
         }
     }
 
+
+
     private Statistic getNext(Label labelNumber)
     {
+
         return accommodationType; //Just some bs to make it run
     }
 
+    private Statistic getPrev(Label labelNumber){
+
+        return accommodationType; //Just some bs to make it run
+
+    }
     private void displaySinglePanel(Label titleLbl, Label textLbl, Statistic stat) // Get correct stat at runtime
     {
         titleLbl.setText(stat.getName());
