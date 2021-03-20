58bb491a571355a4c7a1daba143ba662ab42a45a
Barnabas Szalai <barnusszalai@gmail.com>
2021-03-20 16:20:23 +0100

    Login System Created

--- CW4 - EarthBnB/src/project_files/LoginPanelController.java
+++ CW4 - EarthBnB/src/project_files/LoginPanelController.java
@@ -1,9 +1,16 @@
 package project_files;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -18,9 +25,44 @@ public class LoginPanelController implements Initializable {
     @FXML
     TextField passwordTextField;
 
+    @FXML
+    BorderPane mainPane;
+
+    @FXML
+    BorderPane loginpagePane;
+
+    @FXML
+    Button signinMenu;
+
+    @FXML
+    Button signupMenu;
+
+    private int accountID;
+
+    private boolean currentPanel;
+
     public void loginButtonClicked(javafx.event.ActionEvent actionEvent) {
         validateLogin();
 
+    }
+
+    public void navigateToRegister(javafx.event.ActionEvent actionEvent) throws IOException {
+        if(((Button) actionEvent.getSource()).getId().equals("signupMenu")) {
+            Parent nextPanel;
+            FXMLLoader statsLoader = new FXMLLoader(getClass().getResource("registerView.fxml"));
+            nextPanel = statsLoader.load();
+            loginpagePane.setCenter(nextPanel);
+            signinMenu.setStyle(signupMenu.getStyle());
+            signupMenu.setStyle("-fx-background-color: #FF5733");
+        } else {
+            Parent root = FXMLLoader.load(getClass().getResource("loginPanel.fxml"));
+            Stage newStage = new Stage();
+            newStage.setTitle("Login Panel");
+            newStage.setScene(new Scene(root, 600, 500));
+            newStage.setResizable(false);
+            newStage.show();
+            loginpagePane.getScene().getWindow().hide();
+        }
     }
 
 
@@ -37,7 +79,8 @@ public class LoginPanelController implements Initializable {
 
        if(queryResult.next()) {
             System.out.println("Logged In");
-            System.out.println(queryResult.getInt("accountID"));
+            accountID = queryResult.getInt(1);
+            System.out.println("Account ID:" + accountID);
         } else {
            System.out.println("Incorrect Login Details");
        }
@@ -51,6 +94,7 @@ public class LoginPanelController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        currentPanel = false;
+        signinMenu.setStyle("-fx-background-color: #FF5733");
     }
 }
---
+++ CW4 - EarthBnB/src/project_files/RegisterController.java
@@ -0,0 +1,65 @@
+package project_files;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class RegisterController {
+
+    @FXML
+    TextField nameField;
+    @FXML
+    TextField emailField;
+    @FXML
+    TextField pwField;
+    @FXML
+    TextField pwConfField;
+    @FXML
+    Label feedbackLabel;
+
+
+
+
+
+    public void validateRegister() {
+        DatabaseConnection connection = new DatabaseConnection();
+        Connection connectDB = connection.getConnection();
+
+        String createSignup = "INSERT INTO account VALUES (NULL, '" + nameField.getText() + "', '" +  pwField.getText() + "', '" + emailField.getText() + "')";
+        String checkSignup = "SELECT * FROM account WHERE username = '"+ nameField.getText() + "'";
+        if (nameField.getText().length() != 0 && pwField.getText().length() != 0 && pwField.getText().length() != 0 && pwConfField.getText().length() != 0) {
+            if ((pwField.getText().equals(pwConfField.getText()))) {
+                try {
+                    Statement statement = connectDB.createStatement();
+                    ResultSet queryResult = statement.executeQuery(checkSignup);
+                    if(queryResult.next()) {
+                        feedbackLabel.setText("Account with this name exists!");
+                        feedbackLabel.setTextFill(Color.RED);
+                    } else {
+                        statement.executeUpdate(createSignup);
+                        feedbackLabel.setText("Successful registration!");
+                        feedbackLabel.setTextFill(Color.GREEN);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    e.getCause();
+                    feedbackLabel.setText("The account could not be created!");
+                    feedbackLabel.setTextFill(Color.RED);
+                }
+            } else {
+                feedbackLabel.setText("The two passwords don't match!");
+                feedbackLabel.setTextFill(Color.RED);
+            }
+        } else {
+            feedbackLabel.setText("Please fill in all fields!");
+            feedbackLabel.setTextFill(Color.RED);
+        }
+    }
+
+
+}
